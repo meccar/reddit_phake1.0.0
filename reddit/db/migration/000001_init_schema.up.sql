1ea65a4b700a5d1596bbd696c61c4633ec5fc560
Tung Doan <tung14hn@gmail.com>
2024-03-05 03:40:36 +0000

    updated migrateup file

--- reddit/db/migration/000001_init_schema.up.sql
+++ reddit/db/migration/000001_init_schema.up.sql
@@ -1,3 +1,15 @@
+CREATE TABLE Account (
+  id uuid PRIMARY KEY,
+  role VARCHAR NOT NULL,
+  username VARCHAR NOT NULL,
+  password VARCHAR NOT NULL,
+  is_email_verified bool NOT NULL DEFAULT false,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Added unique constraint to the username column
+ALTER TABLE Account ADD CONSTRAINT unique_username UNIQUE (username);
+
 CREATE TABLE Form (
   id uuid PRIMARY KEY,
   viewer_name VARCHAR NOT NULL,
@@ -11,44 +23,30 @@ CREATE TABLE Post (
   title VARCHAR NOT NULL,
   article TEXT NOT NULL,
   picture BYTEA,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TYPE UserRole AS ENUM ('guest', 'user', 'admin');
-
-CREATE TABLE Account (
-  id uuid PRIMARY KEY,
-  role UserRole NOT NULL,
-  username VARCHAR NOT NULL,
-  password VARCHAR NOT NULL,
-  is_email_verified bool NOT NULL DEFAULT false,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+  username VARCHAR NOT NULL, 
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (username) REFERENCES Account (username)
 );
 
 CREATE TABLE Session (
   id uuid PRIMARY KEY,
   username VARCHAR NOT NULL,
-  -- token VARCHAR(500) NOT NULL,
   role VARCHAR(5) NOT NULL,
   expires_at TIMESTAMPTZ,
-  -- DEFAULT CURRENT_TIMESTAMP + INTERVAL '1 hour',
-  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (username) REFERENCES Account(username)
 );
 
 CREATE TABLE Verify_email (
-  -- id uuid PRIMARY KEY,
-  username varchar NOT NULL PRIMARY KEY,
-  secret_code varchar NOT NULL,
+  username VARCHAR NOT NULL PRIMARY KEY,
+  secret_code VARCHAR NOT NULL,
   is_used bool NOT NULL DEFAULT false,
   expires_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP + INTERVAL '15 minutes',
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Added foreign key constraint to Verify_email table
 ALTER TABLE Verify_email ADD FOREIGN KEY (username) REFERENCES Account (username);
 
--- ALTER TABLE Account ADD COLUMN is_email_verified bool NOT NULL DEFAULT false;
-
-ALTER TABLE Session ADD FOREIGN KEY (username) REFERENCES Account(username);
-
+-- Added foreign key constraint to Post table
 ALTER TABLE Post ADD FOREIGN KEY (username) REFERENCES Account (username);
-
